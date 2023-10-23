e46a18d1d50ad28f4b72473deb1e441a45859070
Guilherme Silva <guilhermemas2004@gmail.com>
2023-10-23 11:58:17 +0100

    changes

--- enemies.js
+++ enemies.js
@@ -10,25 +10,33 @@ class Enemies {
         this.element.style.left = `${this.left}px`;
         this.gameScreen.appendChild(this.element);
         this.direction = 1;
+        this.boundary = this.gameScreen.offsetWidth - this.width;
+        this.scaleFactor = 1.2;
+        /* this.health = 50;
+        this.damage=5;
+        this.isCollapsing= false; */
     }
         
     updatePosition(){
         this.element.style.left = `${this.left}px`;
     }
-        
+
     move(){
         if (this.left <= 0) {
             this.direction = 1;
-            this.element.style.transform = `scale(${this.width + 50}, ${this.height + 50})`;
-        } else if (this.left >= window.innerWidth - this.width) {
-            this.direction = -1;
-            this.height += 50;
             this.width += 50;
-            this.element.style.transform = `scale(${this.width + 50}, ${this.height + 50})`;
+            this.height += 50;
+            this.element.style.transform = `scale(${this.width}px, ${this.height}px)`;
+        } else if (this.left >= this.boundary) {
+            this.direction = -1;
+            this.width += 50;
+            this.height += 50;
+            this.element.style.transform = `scale(${this.width}px, ${this.height}px)`;
         }
 
         this.left += this.direction;
         this.updatePosition();
+        requestAnimationFrame(() => this.move());
     }
 }
 
