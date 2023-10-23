5b0d459b799fb8a541077ba9a474a6b9ffc93a67
Guilherme Silva <guilhermemas2004@gmail.com>
2023-10-23 11:42:32 +0100

    new changes

--- enemies.js
+++ enemies.js
@@ -1,4 +1,4 @@
-class Enemies {
+class Enemies {   
     constructor(gameScreen) {
         this.gameScreen = gameScreen;
         this.element = document.createElement("img");
@@ -6,19 +6,25 @@ class Enemies {
         this.left = 0;
         this.width = 100;
         this.height = 100;
+        this.element.style.position = "absolute";
         this.element.style.left = `${this.left}px`;
         this.gameScreen.appendChild(this.element);
+        this.direction = 1;
     }
+        
     updatePosition(){
         this.element.style.left = `${this.left}px`;
     }
+        
     move(){
-        if (this.left > window.innerWidth - this.width){
-            this.left--
+        if (this.left <= 0) {
+            this.direction = 1;
+        } else if (this.left >= window.innerWidth - this.width) {
+            this.direction = -1;
         }
-        else if (this.right > window.innerWidth + this.width){
-            this.left++
-        }
-        this.updatePosition()
+
+        this.left += this.direction;
+        this.updatePosition();
     }
 }
+    
